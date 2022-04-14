e5ee35fe9874592cc5bc61e6933ed62164602190
chello1975 <chello1975@example.com>
2022-04-14 16:54:53 -0300

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
-  title = 'PrimeraApp';
+  title:string = 'PrimeraApp';
+  todo :string= '';
+  todoArray:any=[];
+  addTodo(value:any)
+     {this.todoArray.push(value);
+      console.log(this.todoArray);
+     }
+     deleteItem(todo:any)
+     {   
+        for(let i=0 ;i<= this.todoArray.length ;i++)
+        {    if(todo== this.todoArray[i])
+           {     this.todoArray.splice(i,1)   
+           }
+        }
+    }
+
 }
